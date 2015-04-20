4ed3f7413dc7e9c3c5c5825aeb20acddc5c2ff7e
cherudim9 <cherudim9@gmail.com>
2015-04-20 11:58:37 +0800

    Load obj files and render a dinosaur

---
+++ include/object_descripter.h
@@ -0,0 +1,59 @@
+#ifndef _OBJECT_DESCRIPTER_H
+#define _OBJECT_DESCRIPTER_H
+
+#pragma once
+#include <cassert>
+#include "basic_geometry.h"
+#include <vector>
+
+namespace ObjectDescripter
+{
+    template <typename T, int N> class Array 
+    {
+    public:
+      enum {_len = N};
+      typedef T t_Val; 
+    public:
+      T& operator[] (int i)
+        {
+	  assert(i>=0&&i<N);
+	  return _p[i];
+        }
+      const T& operator[] (int i) const 
+      {
+	assert(i>=0&&i<N);
+	return _p[i];
+      }
+
+    protected:
+      T _p[N];
+    };
+
+    class ObjectT
+    {
+    public:
+        ObjectT(void);
+        ~ObjectT(void);
+        
+    public:
+        bool IsLoaded() { return m_pVertexList!=NULL;}
+
+        void Destroy();
+        bool LoadFromObj(const char* fn);
+        bool SaveToObj(const char* fn);
+        
+    protected:
+        bool Parse(FILE* fp);
+        bool CheckParse(int nVertices,std::vector<Array<int,3> > & vecTriangles);
+
+    public:
+
+        int             m_nVertices;
+        int             m_nTriangles;
+        PointT*          m_pVertexList;
+        Array<int,3>*   m_pTriangleList;
+    };
+
+}
+
+#endif
